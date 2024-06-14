7ed19da5ec7e1ec7b8fc67f9583088b391b5cfc6
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-13 22:04:15 -0500

    Función para invertir números

--- ejercicio_02_lista.py
+++ ejercicio_02_lista.py
@@ -1,1 +1,14 @@
 # Escribir un programa que almacene en una lista los números del 1 al 10 y los muestre por pantalla en orden inverso separados por comas.
+
+numeros = []
+
+def invertir_lista(lista):
+    for num in range(1, 10 + 1):
+        lista.append(num)
+    
+    lista.reverse()
+    return lista
+
+num_reves = invertir_lista(numeros)
+
+print(num_reves)
