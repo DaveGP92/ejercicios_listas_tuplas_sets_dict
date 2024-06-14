4b9e3c2e8b14be5c8ffed9741f6efb694b3385ec
Dave González Perdomo <dave.gonzalez1992@outlook.com>
2024-06-13 20:16:18 -0500

    Agregando funciones y variables

--- ejercicio_01_lista.py
+++ ejercicio_01_lista.py
@@ -1,1 +1,36 @@
 # Escribir un programa que almacene las asignaturas de un curso (por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista, pregunte al usuario la nota que ha sacado en cada asignatura, y después las muestre por pantalla con el mensaje En <asignatura> has sacado <nota> donde <asignatura> es cada una des las asignaturas de la lista y <nota> cada una de las correspondientes notas introducidas por el usuario.
+
+asignaturas = []
+notas = []
+materias_mas_nota = {}
+print("Puedes elegir un máximo de cinco (5) asignaturas:")
+
+def agregar_asignatura():
+    
+    for i in range(1, 5 + 1):
+        
+        asignatura_elegida = input(f"Escribe la asignatura número {i} para agregar a la lista:\n")
+        asignaturas.append(asignatura_elegida)
+        
+    return asignaturas
+
+
+def preguntar_notas():
+    
+    for materia in asignaturas:
+        nota = float(input(f"¿Qué notas sacaste en {materia}?\n"))
+        notas.append(nota)
+        
+    return notas
+
+def mostrar_info():
+    materias_mas_nota = dict(zip(asignaturas, notas))
+
+
+agregar_asignatura()
+
+preguntar_notas()
+
+#materias_mas_nota = dict(zip(asignaturas, notas)) ---> de esta forma creamos un diccionario a partir de dos listas.
+
+mostrar_info()
