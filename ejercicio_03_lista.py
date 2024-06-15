1ec99b0b2f7a59a6e4869ba00c45144fbd4dd169
Dave <dave.gonzalez1992@outlook.com>
2024-06-15 17:14:53 -0500

    ciclo for

--- ejercicio_03_lista.py
+++ ejercicio_03_lista.py
@@ -1,3 +1,33 @@
 # Escribir un programa que almacene el abecedario en una lista, elimine de la lista las letras que ocupen posiciones múltiplos de 3, y muestre por pantalla la lista resultante.
 
-abc = []
+from string import ascii_lowercase
+
+abc = list(ascii_lowercase)
+nueva_lista = []
+
+
+
+for i in range(0, len(abc)):
+        if (i + 1) % 3 != 0:
+            nueva_lista.append(abc[i])
+
+        else:
+            continue
+
+
+
+print(f"nueva lista {nueva_lista}")
+            
+    
+
+
+
+
+
+
+
+
+        
+        
+
+
